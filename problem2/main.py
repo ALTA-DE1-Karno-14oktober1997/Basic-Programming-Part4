35675c2b86f735715599a8446358492a9e69a4e7
Karno14 <karnopunta29@gmain.com>
2023-10-08 00:47:26 +0700

    tambah solusi

--- problem2/main.py
+++ problem2/main.py
@@ -1,5 +1,16 @@
 def draw_xyz(N):
     pattern = ""
+    k=1
+    for i in range (1,N+1):
+        for j in range (1,N+1):
+            if k%3==0:
+                pattern+= 'X '
+            elif k%2==0:
+                pattern+= 'Z '
+            else:
+                pattern+= 'Y '
+            k+=1
+        pattern +='\n'
     return pattern
 
 if __name__ == '__main__':
