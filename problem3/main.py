53914dba0a854dc64eb708ee5fadd4e843e1f7ae
Karno14 <karnopunta29@gmain.com>
2023-10-08 01:35:54 +0700

    menambahkan solusi

--- problem3/main.py
+++ problem3/main.py
@@ -1,9 +1,14 @@
 def cetak_table_perkalian(number):
     pattern = ""
+    for i in range(1, number + 1):
+        for j in range(1, number + 1):
+            result = i * j
+            pattern += f'{result} '
+        pattern += '\n'
     return pattern
 
 if __name__ == '__main__':
-    cetak_table_perkalian(9)
+    print(cetak_table_perkalian(9))
     """
      1 2 3 4 5 6 7 8 9
      2 4 6 8 10 12 14 16 18
